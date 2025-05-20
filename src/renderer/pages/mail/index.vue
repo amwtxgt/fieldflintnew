a2956b9a2f94ae1b030f3827688b8c2f60db7dd7
amwtxgt <88107477@qq.com>
2025-05-20 10:12:17 +0800

    feat(env): 添加环境变量配置并更新相关文件
    - 新增 .env.development 和 .env.production 文件，配置不同环境下的 API URL 和其他参数
    - 在 mail 页面中添加环境变量测试输出
    - 更新 package.json，增加新的构建和开发脚本
    - 在 vite-env.d.ts 中定义新的环境变量类型和接口

--- src/renderer/pages/mail/index.vue
+++ src/renderer/pages/mail/index.vue
@@ -2,12 +2,14 @@
 
 	import {defineAsyncComponent} from "vue";
 	const abc = defineAsyncComponent(() => import('./components/abc.vue'))
-
+	console.log(import.meta.env.VITE_HOME)
 </script>
 
 <template>
 	<div class="main">
 		mail
+		<h1>Vite is running in %MODE%</h1>
+		<p>Using data from %VITE_API_URL%</p>
 		<abc></abc>
 	</div>
 </template>
--- src/renderer/vite-env.d.ts
+++ src/renderer/vite-env.d.ts
@@ -1,1 +1,21 @@
 /// <reference types="vite/client" />
+
+interface ViteTypeOptions {
+	// 添加这行代码，你就可以将 ImportMetaEnv 的类型设为严格模式，
+	// 这样就不允许有未知的键值了。
+	// strictImportMetaEnv: unknown
+}
+
+interface ImportMetaEnv {
+	// 更多环境变量...
+	VITE_DOMAIN: string
+	VITE_BASE_API: string
+	VITE_TICKET_URL: string
+	VITE_BBS_URL: string
+	VITE_EXTENSION_URL: string
+	VITE_HOME: string
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv
+}
